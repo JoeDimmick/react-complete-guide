b7d304be3d5412848f5a8475e00972c282c928c8
Joe <JoeWDimmick@gmail.com>
2024-12-21 13:51:47 -0700

    3.55 complete, working tabs with dynamic content.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,19 +1,32 @@
-import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from './assets/data/data-with-examples.js';
 import Header from './components/header/Header.jsx';
 import CoreConcept from './components/coreconcepts/CoreConcept.jsx'
 import TabButton from './components/tabmenu/TabButton.jsx';
-import { useState } from 'react';
 
 function App() {
 
-  //let tabContent = 'Please select a tab.'
-  const [tabContent, setTabContent] = useState('Please select a tab.')
+  /*
+    useState is a react hook provided by react. tells react when data is changed and will rerender when data is changed.
+    !Important! only call hooks inside of Component Functions and only call Hooks on the top level. (not inside other inner functions)
+  */
+  const [selectedTopic, setSelectedTopic] = useState('components');
+  /*
+    useState() accepts an argument. The default value you want stored. The value that should be use when first rendered.
+    useState always returns an array of 2 elements
+      0: the currrent value of a variable for the current render cycle
+      1: a function provided by react that updates the value of the first index and rerenders the component.
+  */
 
   function handleSelect(selectedButton) {
-
-    setTabContent(selectedButton);
-    console.log(selectedButton);
-}
+    setSelectedTopic(selectedButton);
+    /*
+       when you call setSelectedTopic react "schedules" the rerender, then re-executes the component function the value is only available after
+       the render is completed. so console logging after the call will result in the "previous" value being displayed in the console.
+    */
+    console.log(selectedTopic);
+  }
 
   return (
     <div>
@@ -36,12 +49,20 @@ function App() {
               Example of passing handleSelect to the TabButton Component when the button is clicked: <TabButton onSelect={handleSelect} label="JSX"/>
               Example of using an anonymous function to execute handleSelect when the button is clicked: <TabButton onSelect={() => handleSelect()} label="Components"/>
            */}
-            <TabButton label='Components' onSelect={() => handleSelect('Components')} />
-            <TabButton label='JSX' onSelect={() => handleSelect('JSX')}/>
-            <TabButton label='Props' onSelect={() => handleSelect('Props')}/>
-            <TabButton label='State'onSelect={() => handleSelect('State')}/>
+            <TabButton label='Components' onSelect={() => handleSelect('components')} />
+            <TabButton label='JSX' onSelect={() => handleSelect('jsx')}/>
+            <TabButton label='Props' onSelect={() => handleSelect('props')}/>
+            <TabButton label='State'onSelect={() => handleSelect('state')}/>
           </menu>
-          {tabContent}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
