ab820492cf8b4232824b7ca2c7a6443329ccedd5
Joe <JoeWDimmick@gmail.com>
2024-12-21 11:58:04 -0700

    3.50 complete. some small refactors and started the TabMenu/TabButton. First event listener created.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/header/Header.jsx';
 import CoreConcept from './components/coreconcepts/CoreConcept.jsx'
+import TabMenu from './components/tabmenu/TabMenu.jsx';
 
 function App() {
   return (
@@ -15,6 +16,15 @@ function App() {
             <CoreConcept title={ CORE_CONCEPTS[2].title } description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image} />
             <CoreConcept title={ CORE_CONCEPTS[3].title } description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabMenu label="Components"/>
+            <TabMenu label="JSX"/>
+            <TabMenu label="Props"/>
+            <TabMenu label="State"/>
+          </menu>
         </section>
       </main>
     </div>
---
+++ 01-starting-project/src/components/tabmenu/TabMenu.jsx
@@ -0,0 +1,23 @@
+import './Tabmenu.css';
+
+export default function TabMenu({label}) {
+    //pure javascript way to handle onclick events
+    //document.querySelector('button').addEventListener('click', () => { do something interesting});
+    //document.querySelector('button') querySelector would grab all elements of 'button'
+    //.addEventListener() is a built in function that accepts 2 arguments (event type, and a function to execute when the event trigger happens)
+    function handleClick() {
+        console.log('sup');
+    }
+     
+
+    return (
+        <li>
+            <button onClick={handleClick}>{label}</button>
+            {/*handleClick() vs handleclick */}
+            {/* handleClick means the function would be executed on render */}
+            {/* What really needs to happen is the function needs to be executed when the button is clicked. Meaning the function needs to passed (handed off to) 
+                the button via the 'onClick' Attribute to be executed when the button is clicked.*/}
+
+        </li>
+    )
+}
