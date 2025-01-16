c7dd7812d76be04ee1362685e64e33ab4507d12e
Joe <JoeWDimmick@gmail.com>
2025-01-16 16:31:38 -0700

    started adding player functionality

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -7,6 +7,22 @@ function App() {
     <>
       <h1>React Tic-Tac-Toe</h1>
       <Header/>
+
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <li>
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </li>
+            <li>
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </li>
+          </ol>
+          GAME BOARD
+        </div>
+      </main>
     </>
     )
 }
